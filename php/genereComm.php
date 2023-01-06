124fd7470af2369741eea904d7ce2051ec366d2d
Souhaila84 <souhaila.moumane@etu.univ-amu.fr>
2023-01-06 14:18:57 +0100

    ajout ajax dans html(afficher comm sur le site)

--- php/genereComm.php
+++ php/genereComm.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once 'includes/dbh.inc.php';
+    require_once '../includes/dbh.inc.php';
     $query = "SELECT libelle, pseudo FROM commGenere ORDER BY rand()";
     $result = mysqli_query($bdd, $query);
     $comment = mysqli_fetch_assoc($result);
