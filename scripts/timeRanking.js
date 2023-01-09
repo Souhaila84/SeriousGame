8cc0f06489535b49318b4da76be17b2c02e5a4fb
VictorBouveret <victor.bouveret@etu.univ-amu.fr>
2023-01-09 16:26:09 +0100

    Finished commenting the timeRanking page

--- scripts/timeRanking.js
+++ scripts/timeRanking.js
@@ -1,14 +1,25 @@
 /** 
- *  @fileOverview This file contains 1 class, timeRankingPage. This class represent the timeRanking page and there also is the timePlayed var that imported from "findTheMurdererGame.js"
- *  
+ *  @fileOverview This file contains 1 class, timeRankingPage. This class represent the timeRanking page"
  *
  *  @author Bouveret Victor
  */
 
 export {timeRankingPage}; 
 
+
+/** 
+ *  @fileOverview This file contains 1 import, it is the timePlayed var from "findTheMurdererGame.js".
+ *
+ *  @author Bouveret Victor
+ */
 import {timePlayed} from "./findTheMurdererGame.js";
 
+
+/**
+ * This class is the last scene of our game, here the user will see how much time did he spent on his run.
+ * @author Bouveret Victor
+ * @extends Phaser.Scene
+ */
 class timeRankingPage extends Phaser.Scene {
 
     constructor () {
@@ -21,40 +32,116 @@ class timeRankingPage extends Phaser.Scene {
 
     create() {
         
-        var secondes = ((timePlayed / 1000)% 60);
+        /**
+         * here we are creating two variables, seconds and minutes, we are making some maths to have a good and nice looking time at the end, and after that we end up with the playingTimeDisplay that is our final var to see how much time did the user spend on his run.
+         * @author Bouveret Victor
+         */
+        var seconds = ((timePlayed / 1000)% 60);
         var minutes = (Math.floor((timePlayed / 1000) / 60));
-        var affichageMinutesSecondes = (minutes + " min et " + secondes + " secondes");
-        console.log(affichageMinutesSecondes);
-        //Rules Part
-        //adding the rules background
+        var playingTimeDisplay = (minutes + " min et " + seconds + " secondes");
+        console.log(playingTimeDisplay);
+        
+        
+        /**
+         * adding the background to the timeRankingPage scene.
+         * @author Bouveret Victor
+         * @name timeRankingground
+         * @name {Phaser.GameObjects.Image}
+         */
         var timeRankingground = this.add.image(400,300, 'timeRankingBackground');
         
-        //adding start container to the rules screen
-        
+
+        /**
+         * this is the text that will be in the timeRankingContainer.
+         * @author Bouveret Victor
+         * @name timeRankingText
+         * @type {Phaser.GameObjects.Text}
+         */
         var timeRankingText = this.add.text(-84,-13, "Return to Menu",{ fontSize : 24 , fontFamily: 'Georgia, Times, serif'});
+        
+        
+        /**
+         * this is the rectangle that will be in the timeRankingContainer.
+         * @author Bouveret Victor
+         * @name timeRankingRect
+         * @type {Phaser.Geom.Rectangle}
+         */
         var timeRankingRect = this.add.rectangle(0,0,200,50,0x7b6c4f, 0.8);
         timeRankingText.setTint(0xc2baac);
 
+        
+        /**
+         * this is the rectangle that display an effect with a border arround the timeRankingRect in the timeRankingContainer.
+         * @author Bouveret Victor
+         * @name timeRankingRectStyle
+         * @type {Phaser.Geom.Rectangle}
+         */
         var timeRankingRectStyle = this.add.rectangle(0,0,200,50);
         timeRankingRectStyle.setStrokeStyle(2,0x000000);
         
+        
+        /**
+         * this is the first container that will allow the user to click on "Return to Menu".
+         * @author Bouveret Victor
+         * @name timeRankingContainer
+         * @type {Phaser.GameObjects.Container}
+         */
         var timeRankingContainer = this.add.container(400,450,[timeRankingRect ,timeRankingText,timeRankingRectStyle]);
         timeRankingContainer.setInteractive(new Phaser.Geom.Rectangle(-100,-25,200,50), Phaser.Geom.Rectangle.Contains);   
         
-        //adding the rules in the rules screen
+        
+        
+         /**
+         * this is the text that will be in the rankingContainer.
+         * @author Bouveret Victor
+         * @name rankingText
+         * @type {Phaser.GameObjects.Text}
+         */
         var rankingText = this.add.text(-260,-135, "Alec6 : 10.08 minutes",{ fontSize : 20, fontFamily: 'Georgia, Times, serif' });
         rankingText.setTint(0xc2baac);
+        
+        
+         /**
+         * this is the rectangle that will be in the rankingContainer.
+         * @author Bouveret Victor
+         * @name rankingRect
+         * @type {Phaser.Geom.Rectangle}
+         */
         var rankingRect = this.add.rectangle(0,0,550,300,0x7b6c4f, 0.9);
         
+        
+        /**
+         * this is the rectangle that display an effect with a border arround the rankingRect in the rankingContainer.
+         * @author Bouveret Victor
+         * @name rankingRectStyle
+         * @type {Phaser.Geom.Rectangle}
+         */
         var rankingRectStyle = this.add.rectangle(0,0,550,300);
         rankingRectStyle.setStrokeStyle(2,0x000000);
         
+        
+         /**
+         * this is the container that will contain the user username and his best time to finish the game.
+         * @author Bouveret Victor
+         * @name rankingContainer
+         * @type {Phaser.GameObjects.Container}
+         */
         var rankingContainer = this.add.container(400,200,[rankingRect ,rankingText,rankingRectStyle]);
         
+        
+         /**
+         * Here, this function will reload our website page to send the user back onto the main menu after timeRankingContainer has been clicked on.
+         * @author Bouveret Victor
+         */
         timeRankingContainer.on("pointerdown", function(){
             location.reload();
         });
         
+        
+        /**
+         * style effects for the timeRankingContainer.
+         * @author Bouveret Victor
+         */
         timeRankingContainer.on('pointerover', function() {
             timeRankingRect.setFillStyle(0xa88c6c,0.8)
         });
