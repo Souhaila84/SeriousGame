3da7784cd733f80bca51968be70ff6c202083aad
Souhaila84 <souhaila.moumane@etu.univ-amu.fr>
2023-02-27 20:16:16 +0100

    connexion à nv bd pour le générateur de comm

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
         $dbServername = "mysql-enqueteroquette.alwaysdata.net"; 
         $dbUsername = "289405_a";
         $dbPassword = "%Admin0!";
-        $dbName = "enqueteroquette_commdb";
+        $dbName = "enqueteroquette_db";
 
         // Create connection
         $bdd = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
