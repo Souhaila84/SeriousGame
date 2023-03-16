182306e327261783191d5be43df401bde4894360
Veuveuno <98808815+alexisMariotti@users.noreply.github.com>
2023-03-16 14:24:59 +0100

    dataAccess created

--- php/Model/DataAccessRead.php
+++ php/Model/DataAccessRead.php
@@ -1,6 +1,6 @@
 <?php
 
-class ConnexionDBRead {
+class DataAccessRead {
 
     private $PDOInstance = null;
 
@@ -9,8 +9,8 @@ class ConnexionDBRead {
     const DEFAULT_SQL_HOST = "mysql-enqueteroquette.alwaysdata.net";
     const DEFAULT_SQL_DTB = "enqueteroquette_db";
 
-    const READ_SQL_USER = '289405_a'; // ATTENTION CHANGER LES LOG ICI C4EST ROOT
-    const READ_SQL_PASS = '%Admin0!';
+    const READ_SQL_USER = '289405_readOnly'; // user with read only perms
+    const READ_SQL_PASS = '%read0!%';
 
     private $readGameCommStatement;
 
@@ -57,7 +57,7 @@ class ConnexionDBRead {
 
         $this->commGeneratedStatement = $this->PDOInstance->prepare("SELECT libelle, pseudo FROM commGenere ORDER BY rand()");
     }
-
+    //static fabric method for limit connexions to the DB
     public static function getInstance()
     {
         if (is_null(self::$instanceRead)) {
@@ -148,97 +148,3 @@ class ConnexionDBRead {
         return $statement;
     }
 }
-
-class ConnexionDBWrite {
-
-    private $PDOInstance = null;
-
-    private static $instanceWrite = null;
-
-    const DEFAULT_SQL_HOST = "mysql-enqueteroquette.alwaysdata.net";
-    const DEFAULT_SQL_DTB = "enqueteroquette_db";
-
-    const WRITE_SQL_USER = '289405_a';
-    const WRITE_SQL_PASS = '%Admin0!';
-
-    private $writeGameCommentStatement;
-
-    private $deleteSessionStatement;
-
-    private $insertSessionStatement;
-
-    private $setProgressLvlStatement;
-
-    private $insertNewAccountStatement;
-
-    private $updatePlayerTimeStatement;
-
-    private function __construct()
-    {
-        $this->PDOInstance = new PDO('mysql:dbname=' . self::DEFAULT_SQL_DTB . ';host=' . self::DEFAULT_SQL_HOST, self::WRITE_SQL_USER, self::WRITE_SQL_PASS);
-
-        $this->writeGameCommentStatement = $this->getPdo()->prepare("INSERT INTO commentaire(id, libellé) VALUES(?, ?);");
-
-        $this->deleteSessionStatement = $this->getPdo()->prepare("DELETE FROM session WHERE idUser = ?");
-
-        $this->insertSessionStatement = $this->getPdo()->prepare("INSERT INTO session(sessionToken, idUser) VALUES(?, ?)");
-
-        $this->setProgressLvlStatement = $this->getPdo()->prepare("UPDATE user SET nv_progression = ? WHERE id = ?");
-
-        $this->insertNewAccountStatement = $this->getPdo()->prepare("INSERT INTO user(pseudo, email, password) VALUES(?, ?, ?)");
-
-        $this->updatePlayerTimeStatement = $this->getPdo()->prepare("UPDATE user SET bestTime=? WHERE id=? AND (bestTime > ? OR bestTime IS NULL)");
-    }
-
-    public static function getInstance()
-    {
-        if (is_null(self::$instanceWrite)) {
-            self::$instanceWrite = new self();
-        }
-        return self::$instanceWrite;
-    }
-
-    public function getPdo(): PDO
-    {
-        return $this->PDOInstance;
-    }
-
-    public function writeGameComment($id, $comment){
-        $statement = $this->writeGameCommentStatement;
-        $statement->execute(array($id, $comment));
-
-        return $statement;
-    }
-
-    public function deleteSession($id){
-        $statement = $this->deleteSessionStatement;
-        $statement->execute(array($id));
-        return $statement;
-    }
-
-    public function insertSession($token, $id){
-        $statement = $this->insertSessionStatement;
-        $statement->execute(array($token,$id));
-        return $statement;
-    }
-
-    public function setProgressLvl($id, $lvl){
-        $statement = $this->setProgressLvlStatement;
-        $statement->execute(array($lvl,$id));
-        return $statement;
-    }
-
-    public function insertNewAccount($pseudo, $email, $hash){
-        $statement = $this->insertNewAccountStatement;
-        $statement->execute(array($pseudo,$email,$hash));
-        return $statement;
-    }
-
-    public function updatePlayerTime($timeValue, $id){
-        $statement = $this->updatePlayerTimeStatement;
-        $statement->execute(array($timeValue,$id,$timeValue));
-        return $statement;
-    }
-}
-
-?>
