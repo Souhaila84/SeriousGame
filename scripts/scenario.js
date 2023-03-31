8d878d8496c85f8822ceae6209104c74fb43856a
alexis <alexis.mariotti@laposte.net>
2023-03-31 21:27:49 +0200

    merged english scenario and switch script from main

---
+++ scripts/scenario.js
@@ -0,0 +1,22 @@
+document.getElementById("entofr").addEventListener("click", function() {
+    let en = document.getElementById("enAnglais");
+    en.hidden = true;
+    let fr = document.getElementById("enFrancais");
+    fr.hidden = false;
+    let btn = document.getElementById("entofr");
+    btn.hidden = true;
+    let btnfr = document.getElementById("frtoen");
+    btnfr.hidden = false;
+
+});
+
+document.getElementById("frtoen").addEventListener("click", function() {
+    let en = document.getElementById("enAnglais");
+    en.hidden = false;
+    let fr = document.getElementById("enFrancais");
+    fr.hidden = true;
+    let btn = document.getElementById("entofr");
+    btn.hidden = false;
+    let btnfr = document.getElementById("frtoen");
+    btnfr.hidden = true;
+});
