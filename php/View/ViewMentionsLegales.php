f4dc1c6ad6db34f6391efccda8a161c5d62fc4b8
alexis <alexis.mariotti@laposte.net>
2023-03-31 21:55:27 +0200

    merged htmls from main

--- php/View/ViewMentionsLegales.php
+++ php/View/ViewMentionsLegales.php
@@ -10,7 +10,22 @@ class ViewMentionsLegales extends View
 
         $this->title= 'Mentions Légales';
 
-        $this->content = "<p>mentions légales</p>";
+        $this->content = "        <h1>Mentions Legales</h1>
+        <h2>Preambule :</h2>
+            Ce site a ete cree dans le cadre d'un projet au sein de l'IUT d'Aix-Marseille site de Gaston Berger (Aix-en-Provence (13)).
+            <br>
+            Derniere mise a jour du site : 31 / 03 / 2023.
+            <br>
+            Les adresses e-mails communiquees, etant des adresses e-mails etudiantes, peuvent ne plus etre valable a partir de juillet 2024. Dans le cas echeant, si le site est mis à jour, elles seront modifiees dans la mesure du possible.
+        <h2>Proprietes intellectuelles :</h2>
+        <ul>
+            <li>Victor BOUVERET : Mail : <a href='mailto:victor.bouveret@etu.univ-amu.fr'>victor.bouveret@etu.univ-amu.fr</a> | L'adresse et le numero de telephone ne souhaitent pas etre communiquees par cette personne.</li>
+            <li>William GOUJON : Mail : <a href='mailto:william.goujon@etu.univ-amu.fr'>william.goujon@etu.univ-amu.fr</a> | L'adresse et le numero de telephone ne souhaitent pas etre communiquees par cette personne.</li>
+            <li>Alexis MARIOTTI : Mail : <a href='mailto:alexis.mariotti@etu.univ-amu.fr'>alexis.mariotti@etu.univ-amu.fr</a> | L'adresse et le numero de telephone ne souhaitent pas etre communiquees par cette personne.</li>
+            <li>Souhaila MOUMANE : Mail : <a href='mailto:souhaila.moumane@etu.univ-amu.fr'>souhaila.moumane@etu.univ-amu.fr</a> | L'adresse et le numero de telephone ne souhaitent pas etre communiquees par cette personne.</li>
+        </ul>
+        <p>Hebergement du site : Alwaysdata <a href='https://www.alwaysdata.com/en/'>redirection vers le site de Alwaysdata</a>.<br>Mentions legales de l'hebergeur : La societe ALWAYSDATA, SARL au capital de 200.000 euros immatriculee au RCS de Paris sous le numero 492 893 490 dont le siege social se trouve 91 rue du Faubourg Saint Honore - 75008 Paris.</p>
+ ";
 
     }
 }
--- php/View/ViewSupport.php
+++ php/View/ViewSupport.php
@@ -10,7 +10,17 @@ class ViewSupport extends View
 
         $this->title= 'Support';
 
-        $this->content = "<p>Voici la page de support</p>";
+        $this->content = "<h1 class='title'><strong>Bienvenue sur la page Support !</strong></h1>
+<h3 class='title'>Si vous venez à rencontrer un problème, n'hésitez pas à nous en faire part !</h3>
+<form src='../php/Support.php' method='post'>
+    <br>
+    <p><input type='text' name='adresse mail' placeholder='Adresse mail :'></p>
+    <p><input type='text' name='nom' placeholder='Nom :'><input type='text' name='object' placeholder='object :'></p>
+    <div id='zone'>
+        <textarea name='probleme' rows= 9 cols= 100 style='max-width: 100%;' placeholder ='Expliquez le problème :'></textarea>
+    </div>
+    <button class='supportButton' type='submit'>Envoyer</button>
+</form>";
 
     }
 }
