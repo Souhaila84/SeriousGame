c13d02476bfef8e11b8038adad353c6c1e6487db
alexis <alexis.mariotti@laposte.net>
2023-04-01 15:34:41 +0200

    tests for UserChecking with phpunit

---
+++ php/test/CommentCheckingTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPUnit\Doctrine\Instantiator\Exception\lib;
+
+use service\CommentChecking;
+use PHPUnit\Framework\TestCase;
+
+class CommentCheckingTest extends TestCase
+{
+
+    public function testReadComment()
+    {
+
+    }
+}
---
+++ php/test/UserInsertionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PHPUnit\Doctrine\Instantiator\Exception;
+
+use service\UserInsertion;
+use PHPUnit\Framework\TestCase;
+
+class UserInsertionTest extends TestCase
+{
+
+    public function testUpdatePlayerTime()
+    {
+
+    }
+
+    public function testInsertSession()
+    {
+
+    }
+
+    public function testInsertAccount()
+    {
+
+    }
+
+    public function testSetProgressLvl()
+    {
+
+    }
+
+    public function testWriteGameComment()
+    {
+
+    }
+
+    public function testDeleteSession()
+    {
+
+    }
+}
