c261beab5d81f582b1323be604340c518c239ef9
alexis <alexis.mariotti@laposte.net>
2023-04-06 16:21:07 +0200

    cleanned useless files

--- php/genereComm.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'configPDO.php';
-    $result = ConnexionDBRead::getInstance()->commGenerated();
-    $comment = $result->fetch();
-    // Affichage du commentaire
-    
-    echo "<div class='comment'> ". $comment->libelle. "</div>" , " <content id='commentPseudo'> ". $comment->pseudo. "</content>";
-?>
